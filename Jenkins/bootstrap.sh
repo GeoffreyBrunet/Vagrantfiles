4fc01cc41267240ffe4f5d68721c9ca7f6ef57ac
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-03-29 18:13:50 +0100

    Bootsrap updated

--- Jenkins/bootstrap.sh
+++ Jenkins/bootstrap.sh
@@ -1,6 +1,18 @@
+#!/bin/sh
+#Written by Brunet Geoffrey
+
+#Verifie operating system is up to date
 sudo apt update && sudo apt -y upgrade
+
+#Add java repository
 sudo add-apt-repository ppa:webupd8team/java
-#sudo apt -y install oracle-java8-installer
-#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-#sudo apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
-#sudo apt install jenkins
+
+#Install Java
+sudo apt install -y oracle-java8-installer
+
+#Add jenkins repository
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
+
+#Install jenkins
+sudo apt install jenkins
