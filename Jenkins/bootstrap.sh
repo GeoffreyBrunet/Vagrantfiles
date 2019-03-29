11e17fe4aaf80b62c95ce3bf195470e3149ebfda
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-03-29 14:44:29 +0100

    Ajout du dossier nagios

---
+++ Jenkins/bootstrap.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt -y upgrade
+sudo add-apt-repository ppa:webupd8team/java
+#sudo apt -y install oracle-java8-installer
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#sudo apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
+#sudo apt install jenkins
