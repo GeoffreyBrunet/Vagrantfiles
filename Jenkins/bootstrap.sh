827789625f8a38fd64e8b7bc895619c733ac140c
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-03-30 13:51:40 +0100

    Jenkins server is OK, files are updated

--- Jenkins/bootstrap.sh
+++ Jenkins/bootstrap.sh
@@ -8,6 +8,7 @@ sudo apt update && sudo apt -y upgrade
 sudo add-apt-repository ppa:webupd8team/java
 
 #Install Java
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 sudo apt install -y oracle-java8-installer
 
 #Add jenkins repository
@@ -15,4 +16,4 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo apt-add-repository "deb https://pkg.jenkins.io/debian-stable binary/"
 
 #Install jenkins
-sudo apt install jenkins
+sudo apt install -y jenkins
