1f39b0b01af962abb374ecedeff733339fe4c272
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-03-31 19:53:34 +0200

    Jenkins server is OK, files are updated

--- Jenkins/bootstrap.sh
+++ Jenkins/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Written by Brunet Geoffrey
+#Written by Brunet Geoffrey and helped by Jenkins documentation.
 
 #Verifie operating system is up to date
 sudo apt update && sudo apt -y upgrade
