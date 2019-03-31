14d09ad28993ed155926a3c253d22a198eb620a5
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-03-31 19:54:44 +0200

    RabbitMQ is OK

--- RabbitMQ/bootstrap.sh
+++ RabbitMQ/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Written by Brunet Geoffrey
+#Written by Brunet Geoffrey and helped by RabbitMQ documentation.
 
 #Verifie operating system is up to date
 sudo apt update && sudo apt -y upgrade
