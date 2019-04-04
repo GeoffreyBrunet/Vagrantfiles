fa8ee3231d091b34c342867b27f9bae991431376
Geoffrey Brunet <geoffrey-brunet89@outlook.fr>
2019-04-04 15:02:21 +0200

    Kubernetes is OK

--- Kubernetes/bootstrap.sh
+++ Kubernetes/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Written by Brunet Geoffrey and helped by Docker documentation.
+#Written by Brunet Geoffrey and helped by Kubernetes documentation.
 
 #Verifie operating system is up to date
 sudo apt update && sudo apt -y upgrade
